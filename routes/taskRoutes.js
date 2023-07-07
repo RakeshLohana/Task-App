0bc1dc20705179eacf5c17364682acd9a6b0e643
RakeshLohana <rakeshlohana15@gmail.com>
2023-07-08 00:19:02 +0500

    3rd commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,7 +4,7 @@ const taskController = require('../controllers/taskController');
 
 router.get('/getTask', taskController.getAllTasks);
 router.post('/createTask', taskController.createTask);
-router.put('/:  taskId', taskController.updateTask);
+router.put('/:taskId', taskController.updateTask);
 router.delete('/:taskId', taskController.deleteTask);
 
 module.exports = router;
